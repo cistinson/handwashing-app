2bf243db18259526315f6a1fd961ce16869db5dc
Chris Stinson <v-chstin@microsoft.com>
2021-07-22 15:30:30 -0700

    Committing html

---
+++ src/Progress.svelte
@@ -0,0 +1,19 @@
+<style>
+  .progress-container{
+    margin: 20px 0;
+    border: solid #999999 1px;
+  }
+  .progress-bar{
+    height: 30px;
+    width: 20%;
+    background-color: green;
+  }
+</style>
+
+<div bp="grid">
+  <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+    <div class="progress-bar">
+      <span class="sr-only">%</span>
+    </div>
+  </div>
+</div>
