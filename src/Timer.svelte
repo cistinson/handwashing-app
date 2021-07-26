8ae993ef85b9b16cc0fc7072b3ffff19a9c5ec16
Chris Stinson <v-chstin@microsoft.com>
2021-07-26 14:08:57 -0700

    Generated progress bar js

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,5 +1,23 @@
 <script>
   import Progress from './Progress.svelte';
+  const totalSeconds = 20;
+  let secondsLeft = totalSeconds;
+  let isRunning = false;
+
+  $:progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+
+  function startTimer(){
+    const timer = setInterval(() => {
+      isRunning = true;
+      secondsLeft -=1;
+      if (secondsLeft == 0){
+        clearInterval(timer);
+        isRunning = false;
+        secondsLeft = totalSeconts;
+      };
+    }, 1000);
+  };
+
 </script>
 
 <style>
@@ -11,17 +29,24 @@
     width: 100%;
     margin: 10px 0;
   }
+  button[disabled]{
+    background-color: #f2f2f2;
+    color: #333;
+    cursor: not-allowed;
+  }
 </style>
 
 <div bp="grid">
   <h2 bp="offset-5@md 4@md 12@sm">
-    Seconds left: 
+    Seconds left: {secondsLeft}
   </h2>
+  <Progress {progress} />
+  <button disabled={isRunning} class="start" on:click={startTimer}>
+    Start
+  </button>
 </div>
 
-<Progress />
 
-<button class="start">
-  Start
-</button>
+
+
 
